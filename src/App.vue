4bf8d99164d7aae09c224a642b2c8c8914665ee8
fernando143 <azarias08@hotmail.com>
2019-06-01 21:53:19 -0300

    terminado regla para 2 digitos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <input type="text" v-model="number">
+    <button @click="start">Traducir</button>
+    <div>
+      <h5>{{ outputNumber }}</h5>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      number: '',
+      lengthNumber: '',
+      outputNumber: '',
+      nativeNumbers: ['れい','いち','に','さん','よん','ご','ろく','なな','はち','きゅう','じゅう']
+    }
+  },
+
+  methods: {
+    start() {
+      this.calculateLength();
+    },
+
+    calculateLength() {
+      let length = this.number.length;
+      this.lengthNumber = length;
+
+      this.selectRule();
+    },
+
+    selectRule() {
+      switch (this.lengthNumber) {
+        case 1:
+          this.ruleOne();
+          break;
+        case 2:
+          this.ruleTwo();
+          break;
+        default:
+          this.outputNumber = 'no tengo idea'
+          console.log(this.lengthNumber)
+          break;
+      }
+    },
+      ruleOne() {
+        let position = this.number
+        this.outputNumber = this.nativeNumbers[position]
+      },
+      ruleTwo() {
+        let positionOfFirstNumber = this.number.indexOf(1);//search the 1 number
+        let valueFirstNumber = this.number.slice(0,1);
+        let restOfNumber = this.number.slice(1);
+        let countFirstForm;
+        let ruleToTest = new RegExp(/([1-9]0)/);
+        let valueTest = ruleToTest.test(this.number);
+
+        // only for those numbers [10,...,90]
+        if(valueTest) {
+          switch (valueTest) {
+          case (this.lengthNumber === 2 && this.number != '10'):
+            this.outputNumber = this.nativeNumbers[valueFirstNumber] + this.nativeNumbers[10];
+            break;
+          case (this.number === '10'):
+            this.outputNumber = this.nativeNumbers[10];
+            break;
+          }
+
+          return
+        }
+
+        if(positionOfFirstNumber === 0) {
+          countFirstForm = true
+        } else {
+          countFirstForm = false
+        }
+
+        if(countFirstForm) {
+          this.outputNumber = this.nativeNumbers[10] + this.nativeNumbers[restOfNumber]
+        } else {
+          this.outputNumber = this.nativeNumbers[valueFirstNumber] + this.nativeNumbers[10] + this.nativeNumbers[restOfNumber]
+        }
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
