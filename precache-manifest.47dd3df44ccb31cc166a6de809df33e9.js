6abc332d34322670b9296367ecf8be42a3b84f29
Fernando Azarías <azarias08@gmail.com>
2020-10-18 20:05:35 -0300

    deploy v0.7.3

---
+++ precache-manifest.47dd3df44ccb31cc166a6de809df33e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "28adacf96018525505f3",
+    "url": "/nihon-numbers/css/app.e27d684d.css"
+  },
+  {
+    "revision": "28adacf96018525505f3",
+    "url": "/nihon-numbers/js/app.f3597a77.js"
+  },
+  {
+    "revision": "8cca5c31158b608a3ed1",
+    "url": "/nihon-numbers/css/chunk-vendors.6fc3a04e.css"
+  },
+  {
+    "revision": "8cca5c31158b608a3ed1",
+    "url": "/nihon-numbers/js/chunk-vendors.e1b76e10.js"
+  },
+  {
+    "revision": "f914779016617fc1fbe5233f028c09e3",
+    "url": "/nihon-numbers/img/profile700.f9147790.jpg"
+  },
+  {
+    "revision": "5c329d361bb71b356950d3979dbaa72f",
+    "url": "/nihon-numbers/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/nihon-numbers/robots.txt"
+  }
+];
